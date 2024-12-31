178964f5505608891aa1e684264c29e6a5c3c5aa
limuen <limuen_dev@163.com>
2024-12-31 14:16:52 +0800

    feat: 🚀 test app tailwind

--- apps/react19-feature/src/App.tsx
+++ apps/react19-feature/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <div className="bg-blue-500 text-white p-4">This is a Tailwind CSS styled component.</div>
+      <div className="py-14 text-3xl md:text-5xl text-center font-bold">React19-Feautre</div>
     </>
   );
 }
